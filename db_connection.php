b21c798dd7152a580f0d8a68e530e8de53b69f16
hmimini <44686295+hmimini@users.noreply.github.com>
2020-12-20 13:54:05 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,6 +1,6 @@
 <?php
 //Connection Varible to connect to database
-$server = "mysql://vtai6x4446z2gup1:n4izema0m1f9cjgh@jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$server = "mysql://jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
 $username = "vtai6x4446z2gup1";
 $password = "n4izema0m1f9cjgh";
 $database = "vkac8ptr4kpcx1ro";
