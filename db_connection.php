442eff47bc91c6d5159d07c7cfcbc95411485b61
hmimini <44686295+hmimini@users.noreply.github.com>
2020-12-20 14:45:41 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,9 @@
 <?php
 //Connection Varible to connect to database
-//$server = "mysql://jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vkac8ptr4kpcx1ro";
-//$username = "vtai6x4446z2gup1";
-//$password = "n4izema0m1f9cjgh";
+$server = "mysql://jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vkac8ptr4kpcx1ro";
+$username = "vtai6x4446z2gup1";
+$password = "n4izema0m1f9cjgh";
 
 //Connnection String to database
-//$mysqli = new mysqli($server, $username, $password);
-$mysqli = new mysqli("mysql://vtai6x4446z2gup1:n4izema0m1f9cjgh@jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vkac8ptr4kpcx1ro");
+$mysqli = new mysqli($server, $username, $password);
 ?>
