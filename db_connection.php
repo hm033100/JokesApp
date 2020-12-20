72282e4fdaabd41055e3721c46c7ed533be72eb5
hmimini <44686295+hmimini@users.noreply.github.com>
2020-12-20 14:31:35 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,8 @@ $server = "mysql://jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "vtai6x4446z2gup1";
 $password = "n4izema0m1f9cjgh";
 $database = "vkac8ptr4kpcx1ro";
+$port = "3306";
 
 //Connnection String to database
-$mysqli = new mysqli($server, $username, $password, $database);
+$mysqli = new mysqli($server, $username, $password, $database, $port);
 ?>
